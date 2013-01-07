00f88c75f151022bf0c6f7065e354b4d4890e200
tatalos <d.adamos@drupal256.com>
2013-01-07 21:43:31 +0000

    a tiny modification

--- platform/lucid256/updates/2013010701/install.sh
+++ platform/lucid256/updates/2013010701/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-/opt/drupal256/bin/init/drupal256-enable-updates
+mkdir /etc/drupal256
+echo lucid256 > /etc/drupal256/platform
+echo 00000000  > /etc/drupal256/version
+cp /opt/drupal256/packages/drupal256update/drupal256update /etc/drupal256
+ln -s /etc/drupal256/drupal256update /etc/cron.daily/
+
 /opt/drupal256/platform/lucid256/updates/2013010701/01.install
