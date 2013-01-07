bccd55c357705993ad443abb660c8c26e113784f
tatalos <d.adamos@drupal256.com>
2013-01-07 22:28:45 +0000

    final thoughts

--- platform/lucid256/updates/2013010701/install.sh
+++ platform/lucid256/updates/2013010701/install.sh
@@ -2,8 +2,7 @@
 mkdir /etc/drupal256
 echo lucid256 > /etc/drupal256/platform
 echo 00000000  > /etc/drupal256/version
-cp /opt/drupal256/packages/drupal256update/drupal256update /etc/drupal256
-ln -s /etc/drupal256/drupal256update /etc/cron.daily/
+ln -s /opt/drupal256/packages/drupal256update /etc/cron.daily/
 
 /opt/drupal256/platform/lucid256/updates/2013010701/01.install
 cp /opt/drupal256/platform/lucid256/updates/2013010701/version /etc/drupal256/
