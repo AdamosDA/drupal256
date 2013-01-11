4130e1f422400582793c4b89c59fee793e4997ca
tatalos <d.adamos@drupal256.com>
2013-01-11 20:02:48 +0000

    2011011102

--- platform/lucid256/updates/2013011102/install.sh
+++ platform/lucid256/updates/2013011102/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 /opt/drupal256/platform/lucid256/updates/2013011101/01.install
-cp /opt/drupal256/platform/lucid256/updates/2013011101/01.ssigupdate  /etc/cron.hourly/ssigupdate
 /etc/cron.hourly/ssigupdate
 cp /opt/drupal256/platform/lucid256/updates/2013011101/version /etc/drupal256/
