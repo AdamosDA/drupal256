7bd6d739d3bbef5d23900715f56cb34cb5e34318
root <dadam@mus.auth.gr>
2016-02-24 10:55:08 +0200

    used awk for vhost-ls

--- bin/menu/vhost-ls.sh
+++ bin/menu/vhost-ls.sh
@@ -4,7 +4,7 @@ VER=$(grep RELEASE /etc/lsb-release |cut -d\= -f2|cut -d\. -f1)
 
         if [ $VER -gt 13 ]
                 then
-                	ls /etc/apache2/sites-enabled/ --ignore=default.conf --ignore=default-ssl.conf --ignore=000-default.conf |cut -d\. -f1-2
+                	ls /etc/apache2/sites-enabled/ --ignore=default.conf --ignore=default-ssl.conf --ignore=000-default.conf |awk '{print $1}' FS=".conf"
                 else
                 	ls /etc/apache2/sites-enabled/ --ignore=default --ignore=default-ssl --ignore=000-default
                 	
